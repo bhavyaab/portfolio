a29f2f6c4209f61f5380bfb8f7e02bad2c4a8e2d
ackenepah <ackenepah@gmail.com>
2016-09-14 09:56:03 -0700

    added a method to remove the class template from the article tag

--- scripts/projects.js
+++ scripts/projects.js
@@ -17,6 +17,6 @@ Project.prototype.html = function() {
   $newProject.find('img').attr('src', this.projectPreview);
   $newProject.find('description_body').text(this.projectDescription);
 
-  $('article').removeClass('templates');
+  $('article').removeClass('template');
   return $newProject;
 };
