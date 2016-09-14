c1f68df5d5c55e74eac7e925b8d5383494f9be72
ackenepah <ackenepah@gmail.com>
2016-09-14 09:53:22 -0700

    built constructor function that has a series of instances. also built the function that pulls the data from projectArticles.js and displays it on the DOM

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,6 +1,6 @@
 var projects = [];
 
-function Projects(options) {
+function Project (options) {
   this.projectTitle = options.projecTitle;
   this.projectCategory = options.projectCategory;
   this.publishedOn = options.publishedOn;
@@ -11,5 +11,9 @@ function Projects(options) {
 Project.prototype.html = function() {
   var $newProject = $('article.template').clone();
 
+  $newProject.find('h2').text(this.projectTitle);
   $newProject.attr('data-category', this.projectCategory);
+  $newProject.find('time').text(this.publishedOn);
+  $newProject.find('img').attr('src', this.projectPreview);
+  $newProject.find('description_body').text(this.projectDescription);
 };
--- scripts/projectsArticles.js
+++ scripts/projectsArticles.js
@@ -3,24 +3,24 @@ var ourLocalData = [
     projectTitle:       'The Angry Beaver',
     projectCategory:    'small business',
     publishedOn:        '2016-10-08',
-    projectPreview:     '../img/projects/theangrybeaver.jpg',
+    projectPreview:     'http://placehold.it/300x150',
     projectUrl:         'http://github.com/ackenepah',
-    projectDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    projectDescription: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>'
   },
   {
     projectTitle:       'Kelly Pattison',
     projectCategory:    'personal',
     publishedOn:        '2016-10-08',
-    projectPreview:     '../img/projects/kellypattison.jpg',
+    projectPreview:     'http://placehold.it/300x150',
     projectUrl:         'http://github.com/ackenepah',
-    projectDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    projectDescription: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>'
   },
   {
     projectTitle:       'First Day of Skeewl',
     projectCategory:    'school project',
     publishedOn:        '2016-10-08',
-    projectPreview:     '../img/projects/firstdayofskeewl.jpg',
+    projectPreview:     'http://placehold.it/300x150',
     projectUrl:         'http://github.com/ackenepah',
-    projectDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    projectDescription: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>'
   },
 ];
