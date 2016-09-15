198a0b16bf68a805bd169d0b29b86b4e7624c6eb
ackenepah <ackenepah@gmail.com>
2016-09-15 11:21:38 -0700

    fixed issue - pages now fade in when nav link is clicked. mispellings...

--- scripts/articleView.js
+++ scripts/articleView.js
@@ -3,8 +3,9 @@ var articleView = {};
 articleView.handleMainNav = function() {
   $('.main_nav').on('click', '.tab', function() {
     $('.tab-content').hide();
-    var view = $(this).attr('data-content');
-    $('#' + view).fadeIn();
+    var grabDataContent = $(this).attr('data-content');
+    $('#' + grabDataContent).fadeIn();
+    console.log(grabDataContent);
   });
   $('.main_nav .tab:first').click();
 };
